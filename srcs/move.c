47e59508f6653ecb0f182caae68aecf6c2c2d600
Corentin <cf.fleuret@gmail.com>
2025-09-02 16:32:23 +0200

    Tout sauf corriger le probleme de background

--- srcs/move.c
+++ srcs/move.c
@@ -6,7 +6,7 @@
 /*   By: cfleuret <cfleuret@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 14:42:36 by cfleuret          #+#    #+#             */
-/*   Updated: 2025/09/02 16:12:47 by cfleuret         ###   ########.fr       */
+/*   Updated: 2025/09/02 16:29:42 by cfleuret         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,9 @@ void	update_pos(t_data *data, int keysym)
 		&& data->player.next_y >= 0 && data->player.next_y < data->map.height
 		&& data->player.next_x >= 0 && data->player.next_x < data->map.width
 		&& (data->tab[(int)(data->player.next_y + 0.2)]
-		[(int)(data->player.next_x + 0.2)] != '1'))
+		[(int)(data->player.next_x + 0.2)] != '1')
+		&& (data->tab[(int)(data->player.next_y - 0.2)]
+		[(int)(data->player.next_x - 0.2)] != '1'))
 	{
 		data->player.pos_x = data->player.next_x;
 		data->player.pos_y = data->player.next_y;
