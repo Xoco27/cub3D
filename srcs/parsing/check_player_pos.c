cd08c858ac9b80543bf52aaa746a3fffbd3b416b
Saurålt <maxime.garsault@gmail.com>
2025-09-10 16:55:45 +0200

    changes

--- srcs/parsing/check_player_pos.c
+++ srcs/parsing/check_player_pos.c
@@ -6,7 +6,7 @@
 /*   By: mgarsaul <mgarsaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 16:48:40 by mgarsaul          #+#    #+#             */
-/*   Updated: 2025/09/09 17:13:56 by mgarsaul         ###   ########.fr       */
+/*   Updated: 2025/09/09 18:59:44 by mgarsaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,11 @@ int	validate_player_position(char **file, int i)
 				|| file[i][j] == 'E' || file[i][j] == 'W')
 				if (!check_player(file, i, j, start, &player_count))
 					return (0);
+			if (file[i][j] != 'N' || file[i][j] != 'S'
+				|| file[i][j] != 'E' || file[i][j] != 'W'
+				|| file[i][j] != '1' || file[i][j] != '0'
+				|| file[i][j] != ' ' || file[i][j] != '\t')
+				return (false);
 			j++;
 		}
 		i++;
